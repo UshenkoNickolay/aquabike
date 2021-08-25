267e5f2a2eec3a528de956922501e588dea6911a
UshenkoNickolay <arnofayt@gmail.com>
2021-08-25 17:28:22 +0300

    added catalog page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,31 @@
 $(function(){
-  
+
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/icons/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/icons/arrow-right.svg" alt=""></button>',
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  $('.tab').on('click', function(e){
+    e.preventDefault();
+
+    $($(this).siblings()).removeClass('tab--active');
+    $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
+  });
+
+  $('.product-item__favorites').on('click', function(){
+    $(this).toggleClass('product-item__favorites--active')
+  });
+  $('.product-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/icons/arrow-black-left.svg" alt=""></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/icons/arrow-black-right.svg" alt=""></button>',
+  });
+
 });
